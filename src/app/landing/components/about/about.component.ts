c83bb8d18d9f5c794d48e39cb328ea44808a5426
Francisco Miguel Biete Banon <fbiete@uoc.edu>
2023-11-12 11:22:53 +0000

    Change: some format

--- src/app/landing/components/about/about.component.ts
+++ src/app/landing/components/about/about.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'landing-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+  standalone: true,
+  imports: [CommonModule, CardModule],
 })
 export class AboutComponent {
   applicationTitle: string = environment.title;
